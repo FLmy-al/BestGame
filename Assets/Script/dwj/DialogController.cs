b26ed12739e43fa95b88a79c409f38b8ac01a0dd
wsdyx00 <2751712557@qq.com>
2026-05-03 14:30:09 +0800

    改崩了

--- Assets/Script/dwj/DialogController.cs
+++ Assets/Script/dwj/DialogController.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class DialogController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public DialogController dialogController;
+    private void OnMouseDown()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
